59e63369c69b2aa2db04638b53fd643a2f5decae
yyf <yyfjlyx@163.com>
2021-12-26 22:48:13 +0800

    15. 三数之和

--- HOT100/src/LeetCode15.java
+++ HOT100/src/LeetCode15.java
@@ -15,23 +15,23 @@ public class LeetCode15 {
             int right = nums.length - 1;
             while (left < right) {
                 int sum = nums[index] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                    continue;
+                }
+
                 if (sum == 0) {
                     List<Integer> list = new ArrayList<>();
                     list.add(nums[index]);
                     list.add(nums[left]);
                     list.add(nums[right]);
                     result.add(list);
-                    left++;
-                    while (nums[left] == nums[left - 1] && left < right) {
-                        left++;
-                    }
                 }
-                if (sum < 0) {
+                left++;
+                while (nums[left] == nums[left - 1] && left < right) {
                     left++;
                 }
-                if (sum > 0) {
-                    right--;
-                }
+
             }
         }
 
