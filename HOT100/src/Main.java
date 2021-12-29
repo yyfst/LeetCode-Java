c3fb7a31a2f49020a980c611e35aa7431e9b96d6
yyf <yyfjlyx@163.com>
2021-12-29 21:09:25 +0800

    19. 删除链表的倒数第 N 个结点

--- HOT100/src/Main.java
+++ HOT100/src/Main.java
@@ -1,17 +1,42 @@
+import common.ListNode;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("-----------test---------------");
-        testThreeSum();
+//        testThreeSum();
+
+        testRemoveNthFromEnd();
 
     }
 
     public static void testThreeSum() {
         int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
-        LeetCode15 lc= new LeetCode15();
-        List<List<Integer>> result =  lc.threeSum(nums);
+        LeetCode15 lc = new LeetCode15();
+        List<List<Integer>> result = lc.threeSum(nums);
         System.out.println(result);
+    }
+
+    public static void testRemoveNthFromEnd() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        LeetCode19 lc = new LeetCode19();
+
+        ListNode res = lc.removeNthFromEnd(node1, 5);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
     }
 }
