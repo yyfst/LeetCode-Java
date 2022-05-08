7db43782d7202b1df61038700f5248a30e6841fa
yyfst <yyfjlyx@163.com>
2022-05-08 16:17:21 +0800

    提交动态规划

--- DP/src/LeetCode152.java
+++ DP/src/LeetCode152.java
@@ -1,6 +1,26 @@
 public class LeetCode152 {
     public int maxProduct(int[] nums) {
+        int len = nums.length;
+        int[] dpMax = new int[len];
+        int[] dpMin = new int[len];
 
-        return 0;
+        dpMax[0] = nums[0];
+        dpMin[0] = nums[0];
+
+        int res = dpMax[0];
+        for (int i = 1; i < len; i++) {
+            if (nums[i] >= 0) {
+                dpMax[i] = Math.max(dpMax[i - 1] * nums[i], nums[i]);
+                dpMin[i] = Math.min(dpMin[i - 1] * nums[i], nums[i]);
+            } else {
+                dpMax[i] = Math.max(dpMin[i - 1] * nums[i], nums[i]);
+                dpMin[i] = Math.min(dpMax[i - 1] * nums[i], nums[i]);
+            }
+
+            res = Math.max(res, dpMax[i]);
+        }
+
+
+        return res;
     }
 }
